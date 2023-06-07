5979893e2e5b1489e3c0f67702137dea73a36705
Brett <89299506+brett1185@users.noreply.github.com>
2023-06-07 15:55:55 -0400

    Add files via upload

--- Models/HogwartsStudent.cs
+++ Models/HogwartsStudent.cs
@@ -1,7 +1,10 @@
 #pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
 namespace Practice.Models;
 public class HogwartsStudent
 {
+    [Required]
+    [MinLength(3)]
     public string Name {get;set;}
     public string House {get;set;}
     public int CurrentYear {get;set;}
